c0aac2c4e53b8b7fabc85ffff239c8d26562d52f
Rick <Rick@RICKTOP>
2023-10-08 01:24:04 -0400

    updates

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from src.models import diagrams
 
 diagr = diagrams.guitar_fretboard(format_='plain')
 
-diagr = diagrams.filter_guitar_fretboard(diagr, ['C', 'E', 'G', 'B'])
-
+#diagr = diagrams.filter_guitar_fretboard(diagr, ['C', 'E', 'G', 'B'])
 
 data = pandas.DataFrame(diagr)
+
+print(data)
+
+ff = diagrams.get_positional_fingering(diagr, 5, 5)
+
+gg = diagrams.filter_guitar_fretboard(ff, ['A', 'C#|Db', 'E', 'G'])
+
+data = pandas.DataFrame(gg)
 
 print(data)
