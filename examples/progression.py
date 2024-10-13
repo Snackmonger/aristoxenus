2b700781a5ab44f384bc904b7dac972b8698540f
Rick <Rick@RICKTOP>
2024-10-13 02:27:09 -0400

    rough example script

--- examples/progression.py
+++ examples/progression.py
@@ -8,4 +8,5 @@ for scale, pattern in HEPTATONIC_SCALES.items():
     chord_scale = HeptatonicScale(scale_name=scale)
     for degree in [6, 2, 5, 1]:
         inversion = random.randint(0, 3)
-        print(chord_scale.tertial_tetrad(degree).invert(inversion).symbol)
+        chord = chord_scale.tertial_tetrad(degree)
+        print(chord.invert(inversion).symbol)
